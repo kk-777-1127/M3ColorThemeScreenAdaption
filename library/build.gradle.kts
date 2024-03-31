8a99a20db6aab790364a17a5c4d2171b9e5d5335
kk__777 <kk__777@hatena.ne.jp>
2024-03-31 23:53:37 +0900

    Set up publishing setting

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,33 +1,54 @@
+import cl.franciscosolis.sonatypecentralupload.SonatypeCentralUploadTask
+
 plugins {
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("jvm")
+    `maven-publish`
+    id("cl.franciscosolis.sonatype-central-upload") version "1.0.2"
+}
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
-android {
-    namespace = "io.kk__777.library"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            pom {
+                name.set(project.name)
+                description.set("Introducing a library that dynamically generates a Color Palette and Theme for your Android app from a single seed color")
+                url.set("https://github.com/kk-777-1127/M3ColorThemeScreenAdaption")
+                licenses {
+                    license {
+                        name.set("Apache License")
+                        url.set("https://github.com/kk-777-1127/M3ColorThemeScreenAdaption/blob/main/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("kk__777")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/kk-777-1127/M3ColorThemeScreenAdaption")
+                }
+            }
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+}
+
+tasks.named<SonatypeCentralUploadTask>("sonatypeCentralUpload") {
+    dependsOn("jar", "sourcesJar", "javadocJar", "generatePomFileForMavenPublication")
+    username = System.getenv("SONATYPE_CENTRAL_USERNAME")
+    password = System.getenv("SONATYPE_CENTRAL_PASSWORD")
+    archives = files(
+        tasks.named("jar"),
+        tasks.named("sourcesJar"),
+        tasks.named("javadocJar"),
+    )
+    pom = file(
+        tasks.named("generatePomFileForMavenPublication").get().outputs.files.single()
+    )
+    signingKey = System.getenv("PGP_SIGNING_KEY")
+    signingKeyPassphrase = System.getenv("PGP_SIGNING_KEY_PASSPHRASE")
 }
