bf1c4733d40a0a974dcb086495ea0a486f6be0b4
kk-777-1127 <141104949+kk-777-1127@users.noreply.github.com>
2024-04-01 13:32:34 +0900

    Update README.md

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm")
+    `maven-publish`
 }
+
+version = project.version.toString()
 
 dependencies {
     implementation(project(":common"))
@@ -9,4 +12,18 @@ dependencies {
     implementation(libs.ksp)
     implementation(libs.kotlin.poet)
     implementation(libs.kotlin.poet.ksp)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                groupId = project.group.toString()
+                artifactId = project.properties["artifactId.compiler"].toString()
+                version = project.version.toString()
+
+                from(components["java"])
+            }
+        }
+    }
 }
